5ed45cce3447acdeab15a2a68419aa3029128bd0
mariogaulbanget <mariogaulbanget@gmail.com>
2026-08-23 01:51:34 +0530

    Update standings-config.js

--- scripts/standings-config.js
+++ scripts/standings-config.js
@@ -1,12 +1,97 @@
 export const leagues = [
-  {id:"epl",name:"English Premier League",country:"England",countryCode:"ENG",espnId:"eng.1"},
-  {id:"laliga",name:"La Liga",country:"Spain",countryCode:"ESP",espnId:"esp.1"},
-  {id:"serie-a",name:"Serie A",country:"Italy",countryCode:"ITA",espnId:"ita.1"},
-  {id:"bundesliga",name:"Bundesliga",country:"Germany",countryCode:"GER",espnId:"ger.1"},
-  {id:"ligue-1",name:"Ligue 1",country:"France",countryCode:"FRA",espnId:"fra.1"},
-  {id:"eredivisie",name:"Eredivisie",country:"Netherlands",countryCode:"NED",espnId:"ned.1"},
-  {id:"primeira-liga",name:"Primeira Liga",country:"Portugal",countryCode:"POR",espnId:"por.1"},
-  {id:"brasileirao",name:"Brasileirão Série A",country:"Brazil",countryCode:"BRA",espnId:"bra.1"},
-  {id:"mls",name:"Major League Soccer",country:"United States",countryCode:"USA",espnId:"usa.1"},
-  {id:"bri-super-league",name:"BRI Super League",country:"Indonesia",countryCode:"IDN",espnId:"idn.1"}
+  {
+    id: "epl",
+    name: "English Premier League",
+    country: "England",
+    countryCode: "ENG",
+    espnId: "eng.1"
+  },
+
+  {
+    id: "laliga",
+    name: "La Liga",
+    country: "Spain",
+    countryCode: "ESP",
+    espnId: "esp.1"
+  },
+
+  {
+    id: "serie-a",
+    name: "Serie A",
+    country: "Italy",
+    countryCode: "ITA",
+    espnId: "ita.1"
+  },
+
+  {
+    id: "bundesliga",
+    name: "Bundesliga",
+    country: "Germany",
+    countryCode: "GER",
+    espnId: "ger.1"
+  },
+
+  {
+    id: "ligue-1",
+    name: "Ligue 1",
+    country: "France",
+    countryCode: "FRA",
+    espnId: "fra.1"
+  },
+
+  {
+    id: "eredivisie",
+    name: "Eredivisie",
+    country: "Netherlands",
+    countryCode: "NED",
+    espnId: "ned.1"
+  },
+
+  {
+    id: "primeira-liga",
+    name: "Primeira Liga",
+    country: "Portugal",
+    countryCode: "POR",
+    espnId: "por.1"
+  },
+
+  {
+    id: "brasileirao",
+    name: "Brasileirão Série A",
+    country: "Brazil",
+    countryCode: "BRA",
+    espnId: "bra.1"
+  },
+
+  {
+    id: "mls",
+    name: "Major League Soccer",
+    country: "United States",
+    countryCode: "USA",
+    espnId: "usa.1"
+  },
+
+  {
+    id: "bri-super-league",
+    name: "BRI Super League",
+    country: "Indonesia",
+    countryCode: "IDN",
+    espnId: "idn.1"
+  },
+
+  {
+    id: "belgian-pro-league",
+    name: "Belgian Pro League",
+    country: "Belgium",
+    countryCode: "BEL",
+    espnId: "bel.1"
+  },
+
+  {
+    id: "english-championship",
+    name: "English Championship",
+    country: "England",
+    countryCode: "ENG",
+    espnId: "eng.2"
+  }
 ];
